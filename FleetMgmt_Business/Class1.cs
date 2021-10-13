ca8dafc62cfdf19ee10bd11dbc157cead6b47d37
ho-colin <colin.meerschman@student.hogent.be>
2021-10-13 09:57:53 +0200

    + Tests & NmrpltVldtr cijfer > 0

--- FleetMgmt_Business/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace FleetMgmt_Business {
-    public class Class1 {
-    }
-}
--- FleetMgmt_xUnit/Validators/Test_NummerplaatValidator.cs
+++ FleetMgmt_xUnit/Validators/Test_NummerplaatValidator.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using FleetManagement.Checkers;
+using FleetMgmt_Business.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace FleetMgmt_xUnit.Validators {
-    class Test_NummerplaatValidator {
+    public class Test_NummerplaatValidator {
+
+        [Fact]
+        public void Test_Alles_Valid() {
+            Assert.True(NummerplaatValidator.isGeldig("1-AAA-123"));
+        }
+
+        [Theory]
+        [InlineData("A-ABC-123")]
+        public void Test_IndexCijferLetter_InValid(string nummerplaat) {
+            var ex = Assert.Throws<NummerplaatException>(() => NummerplaatValidator.isGeldig(nummerplaat));
+            Assert.Equal("Indexcijfer moet van 1 tot en met 9 zijn!", ex.Message);
+        }
+
+        [Theory]
+        [InlineData("0-ABC-123")]
+        public void Test_IndexCijferNummer_InValid(string nummerplaat) {
+            var ex = Assert.Throws<NummerplaatException>(() => NummerplaatValidator.isGeldig(nummerplaat));
+            Assert.Equal("Indexcijfer moet groter zijn dan 0!", ex.Message);
+        }
     }
 }
