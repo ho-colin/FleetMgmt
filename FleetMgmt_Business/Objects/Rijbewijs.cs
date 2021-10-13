59a399d6ac04e19f9a6206bdfcac192d4ffa029b
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2021-10-13 16:32:21 +0200

    + Rijbewijs exceptions, testing

--- FleetMgmt_Business/Objects/Rijbewijs.cs
+++ FleetMgmt_Business/Objects/Rijbewijs.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FleetMgmt_Business.Exceptions;
 
 namespace FleetMgmt_Business.Objects {
     public class Rijbewijs {
@@ -12,7 +13,19 @@ namespace FleetMgmt_Business.Objects {
         public DateTime BehaaldOp { get; set; }
 
         public Rijbewijs(string categorie, DateTime behaaldop) {
+            ZetCategorie(categorie);
+            ZetBehaaldOp(behaaldop);
+        }
+
+        public void ZetCategorie(string categorie) {
+            if (string.IsNullOrWhiteSpace(categorie)) throw new RijbewijsException("Rijbewijs: Categorienaam mag niet leeg zijn!");
             this.Categorie = categorie;
+        }
+
+        public void ZetBehaaldOp(DateTime behaaldop) {
+            //Een ongeldige datum heeft altijd een hashcode 0, wanneer de datum dus de hashcode 0 heeft dan is hij ongeldig!
+            if (behaaldop.GetHashCode() == 0) throw new RijbewijsException("Rijbewijs: Datum heeft geen geldige waarde!");
+            if (behaaldop > DateTime.Now) throw new RijbewijsException("Rijbewijs: Datum mag niet in de toekomst zijn!");
             this.BehaaldOp = behaaldop;
         }
 
