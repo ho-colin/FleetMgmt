c7f01d78198643dbfc42ffe08cec2067b3932f59
ho-colin <colin.meerschman@student.hogent.be>
2021-10-13 11:27:06 +0200

    + Eerste logica

--- FleetMgmt_Business/Objects/Tankkaart.cs
+++ FleetMgmt_Business/Objects/Tankkaart.cs
@@ -7,19 +7,27 @@ using System.Threading.Tasks;
 namespace FleetMgmt_Business.Objects {
     public class Tankkaart {
 
-        public string KaartNummer { get; set; }
+        public string KaartNummer { get; private set; }
 
-        public DateTime GeldigheidsDatum { get; set; }
+        public DateTime GeldigheidsDatum { get; private set; }
 
-        public string Pincode { get; set; }
+        public string Pincode { get; private set; }
 
-        public Bestuurder InBezitVan { get; set; }
+        public Bestuurder InBezitVan { get; private set; }
 
         public Tankkaart(string kaartnummer, DateTime geldigheidsdatum, string pincode, Bestuurder inbezitvan) {
             this.KaartNummer = kaartnummer;
             this.GeldigheidsDatum = geldigheidsdatum;
             this.Pincode = pincode;
             this.InBezitVan = inbezitvan;
+        }
+
+        public void zetBestuurder(Bestuurder bestuurder) {
+            this.InBezitVan = bestuurder;
+        }
+
+        public void verwijderBestuurder() {
+            this.InBezitVan = null;
         }
 
     }
