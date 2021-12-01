e5386345c47b357f5b248e42491a5f3dc2b06188
ho-colin <colin.meerschman@student.hogent.be>
2021-12-01 20:02:22 +0100

    + Fix TankkaarTest (extra constructor param)

--- FleetMgmt_xUnit/Objects/Test_Tankkaart.cs
+++ FleetMgmt_xUnit/Objects/Test_Tankkaart.cs
@@ -1,4 +1,5 @@
-﻿using FleetMgmt_Business.Exceptions;
+﻿using FleetMgmt_Business.Enums;
+using FleetMgmt_Business.Exceptions;
 using FleetMgmt_Business.Objects;
 using System;
 using System.Collections.Generic;
@@ -15,14 +16,16 @@ namespace FleetMgmt_xUnit.Objects {
         [InlineData(-1)]
         public void Test_Kaartnummer_TeLaag_InValid(int kaartnummer) {
             DateTime toekomst = new DateTime(2025, 10, 13);
-            var ex = Assert.Throws<TankkaartException>(() => new Tankkaart(kaartnummer, toekomst, "6969", null, null));
+            List<TankkaartBrandstof> dummyBrandstoffen = new List<TankkaartBrandstof>() { TankkaartBrandstof.Benzine };
+            var ex = Assert.Throws<TankkaartException>(() => new Tankkaart(kaartnummer, toekomst, "6969", null, dummyBrandstoffen, false));
             Assert.Equal("Kaartnummer moet hoger zijn dan 1!", ex.Message);
         }
 
         [Fact]
         public void Test_geldigheidsDatum_Verleden_InValid() {
             DateTime verleden = new DateTime(2020, 10, 15);
-            var ex = Assert.Throws<TankkaartException>(() => new Tankkaart(10, verleden, "6969", null, null));
+            List<TankkaartBrandstof> dummyBrandstoffen = new List<TankkaartBrandstof>() { TankkaartBrandstof.Benzine };
+            var ex = Assert.Throws<TankkaartException>(() => new Tankkaart(10, verleden, "6969", null, dummyBrandstoffen, false));
             Assert.Equal("Geldigheidsdatum moet groter zijn dan vandaag!", ex.Message);
         }
 
@@ -32,7 +35,8 @@ namespace FleetMgmt_xUnit.Objects {
         [InlineData("12A4")]
         public void Test_Pincode_Invalid(string pincode) {
             DateTime toekomst = new DateTime(2025, 10, 13);
-            var ex = Assert.Throws<TankkaartException>(() => new Tankkaart(10, toekomst, pincode, null, null));
+            List<TankkaartBrandstof> dummyBrandstoffen = new List<TankkaartBrandstof>() { TankkaartBrandstof.Benzine };
+            var ex = Assert.Throws<TankkaartException>(() => new Tankkaart(10, toekomst, pincode, null, dummyBrandstoffen, false));
             Assert.Equal("Pincode mag alleen bestaan uit cijfers!", ex.Message);
         }
 
@@ -40,7 +44,8 @@ namespace FleetMgmt_xUnit.Objects {
         public void Test_Pincode_Valid() {
             DateTime toekomst = new DateTime(2025, 10, 13);
             string pincode = "1243";
-            Tankkaart tankkaart = new Tankkaart(10, toekomst, pincode, null, null);
+            List<TankkaartBrandstof> dummyBrandstoffen = new List<TankkaartBrandstof>() { TankkaartBrandstof.Benzine };
+            Tankkaart tankkaart = new Tankkaart(10, toekomst, pincode, null, dummyBrandstoffen, false);
             Assert.Equal(pincode, tankkaart.Pincode);
         }
 
@@ -48,7 +53,8 @@ namespace FleetMgmt_xUnit.Objects {
         public void Test_Kaartnummer_Valid() {
             DateTime toekomst = new DateTime(2025, 10, 13);
             int kaartnummer = 1;
-            Tankkaart tankkaart = new Tankkaart(kaartnummer, toekomst, "6969", null, null);
+            List<TankkaartBrandstof> dummyBrandstoffen = new List<TankkaartBrandstof>() { TankkaartBrandstof.Benzine };
+            Tankkaart tankkaart = new Tankkaart(kaartnummer, toekomst, "6969", null, dummyBrandstoffen, false);
             Assert.Equal(kaartnummer, tankkaart.KaartNummer);
         }
 
@@ -56,7 +62,8 @@ namespace FleetMgmt_xUnit.Objects {
         public void Test_Geblokkeerd_Valid() {
             DateTime toekomst = new DateTime(2025, 10, 13);
             int kaartnummer = 1;
-            Tankkaart tankkaart = new Tankkaart(kaartnummer, toekomst, "6969", null, null);
+            List<TankkaartBrandstof> dummyBrandstoffen = new List<TankkaartBrandstof>() { TankkaartBrandstof.Benzine };
+            Tankkaart tankkaart = new Tankkaart(kaartnummer, toekomst, "6969", null, dummyBrandstoffen, false);
 
             tankkaart.zetGeblokkeerd(true);
 
