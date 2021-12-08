5b3ccd5eef380fb3a79774f2a746458624572b21
ho-colin <colin.meerschman@student.hogent.be>
2021-12-08 10:19:21 +0100

    + MainWindow

--- FleetMgmt_WPF/MainWindow.xaml.cs
+++ FleetMgmt_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FleetMgmt_WPF.TankkaartWindows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,7 @@ namespace FleetMgmt_WPF {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            this.ResizeMode = ResizeMode.NoResize;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
@@ -37,6 +39,12 @@ namespace FleetMgmt_WPF {
         private void Button_Click_2(object sender, RoutedEventArgs e) {
             TypeVoertuigWindow tvw = new TypeVoertuigWindow();
             tvw.Show();
+            this.Close();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e) {
+            TankkaartWindow w = new TankkaartWindow();
+            w.Show();
             this.Close();
         }
     }
