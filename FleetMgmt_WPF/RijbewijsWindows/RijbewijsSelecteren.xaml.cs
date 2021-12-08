6b5578bbb11e5e45cc4e84a9ee0d8ed252b3f9fa
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2021-12-08 10:47:39 +0100

    +BestuurderIssues RijbewijsSelecteerWindow toegevoegd

---
+++ FleetMgmt_WPF/RijbewijsWindows/RijbewijsSelecteren.xaml.cs
@@ -0,0 +1,40 @@
+﻿using FleetMgmt_Business.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FleetMgmt_WPF.RijbewijsWindows {
+    /// <summary>
+    /// Interaction logic for RijbewijsSelecteren.xaml
+    /// </summary>
+    public partial class RijbewijsSelecteren : Window {
+
+        public List<RijbewijsEnum> Rijbewijzen { get; set; }
+
+
+        public RijbewijsSelecteren() {
+            InitializeComponent();
+
+            lstbx_Rijbewijzen.ItemsSource = Enum.GetValues(typeof(RijbewijsEnum));
+        }
+
+        private void btn_Selecteer_Click(object sender, RoutedEventArgs e) {
+            if (Rijbewijzen.Count < 1)
+                MessageBox.Show("Lijst is leeg!", "Selecteer rijbewijs", MessageBoxButton.OK);
+            else 
+                this.Rijbewijzen = lstbx_Rijbewijzen.SelectedItems.Cast<RijbewijsEnum>().ToList();
+            DialogResult = true;
+            this.Close();
+        }
+    }
+}
