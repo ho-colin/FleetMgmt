79f47f978b48e0c6cb4795eaa43f265fafb5381c
ho-colin <colin.meerschman@student.hogent.be>
2021-12-24 14:13:38 +0100

    + connectionString config bestand

--- FleetMgmg_Data/ConnectionClass.cs
+++ FleetMgmg_Data/ConnectionClass.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +10,29 @@ using System.Threading.Tasks;
 namespace FleetMgmg_Data {
     public static class ConnectionClass {
 
-        public static string connectionString = @"workstation id=fleetmgmt.mssql.somee.com;packet size=4096;user id=ddecaluwe_SQLLogin_1;pwd=q76z5jwcr9;data source=fleetmgmt.mssql.somee.com;persist security info=False;initial catalog=fleetmgmt";
-        //public static string connectionString = @"Data Source=DESKTOP-P24EBNO\SQLEXPRESS;Initial Catalog=FleetMgmt;Integrated Security=True";
-
         public static SqlConnection getConnection() {
             try {
-                SqlConnection sqlConnection = new SqlConnection(connectionString);
+                SqlConnection sqlConnection = new SqlConnection(verkrijgConnectionString());
                 return sqlConnection;
             } catch (Exception ex) {
                 throw new ConnectionException("ConnectionClass : ConnectionException - Kon geen verbinding initialiseren!", ex);
             }
+        }
+
+        private static string verkrijgConnectionString() {
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var fleetFolder = Path.Combine(documentPath, "FleetManagement");
+
+            var connectionFile = Path.Combine(fleetFolder + "/connectionString.txt");
+
+            if (!Directory.Exists(fleetFolder)) { Directory.CreateDirectory(fleetFolder); }
+            if(!File.Exists(connectionFile)) {
+                using(StreamWriter sw = File.CreateText(connectionFile)) {
+                    sw.WriteLine(@"workstation id=fleetmgmt.mssql.somee.com;packet size=4096;user id=ddecaluwe_SQLLogin_1;pwd=q76z5jwcr9;data source=fleetmgmt.mssql.somee.com;persist security info=False;initial catalog=fleetmgmt");
+                }
+            }
+
+            return File.ReadLines(connectionFile).First();
         }
     }
 }
