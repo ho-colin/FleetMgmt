b5fffcc3f11cf889329da89317a906f37b5c3c1c
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2022-01-01 15:31:13 +0100

    Klasse die gefilterde resultaten weergeeft toegevoegd

---
+++ FleetMgmt_WPF/Tools/HighlightTextBlock.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace FleetMgmt_WPF.Tools {
+    public class HighlightTextBlock: TextBlock {
+        /*
+         * 
+         * 
+         * 
+         * Klasse die de kans biedt om gefilterde resultaten in kleur weer te geven
+         * 
+         * 
+         * 
+         * 
+         * */
+
+        public new string Text {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public new static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string),
+            typeof(HighlightTextBlock), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.AffectsRender,
+                new PropertyChangedCallback(UpdateHighlighting)));
+
+        public string HighlightPhrase {
+            get { return (string)GetValue(HighlightPhraseProperty); }
+            set { SetValue(HighlightPhraseProperty, value); }
+        }
+
+        public static readonly DependencyProperty HighlightPhraseProperty =
+            DependencyProperty.Register("HighlightPhrase", typeof(string),
+            typeof(HighlightTextBlock), new FrameworkPropertyMetadata(String.Empty, FrameworkPropertyMetadataOptions.AffectsRender,
+                new PropertyChangedCallback(UpdateHighlighting)));
+
+        public Brush HighlightBrush {
+            get { return (Brush)GetValue(HighlightBrushProperty); }
+            set { SetValue(HighlightBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty HighlightBrushProperty =
+            DependencyProperty.Register("HighlightBrush", typeof(Brush),
+            typeof(HighlightTextBlock), new FrameworkPropertyMetadata(Brushes.Yellow, FrameworkPropertyMetadataOptions.AffectsRender,
+                new PropertyChangedCallback(UpdateHighlighting)));
+
+        public bool IsCaseSensitive {
+            get { return (bool)GetValue(IsCaseSensitiveProperty); }
+            set { SetValue(IsCaseSensitiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsCaseSensitiveProperty =
+            DependencyProperty.Register("IsCaseSensitive", typeof(bool),
+            typeof(HighlightTextBlock), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsRender,
+                new PropertyChangedCallback(UpdateHighlighting)));
+
+        private static void UpdateHighlighting(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+            ApplyHighlight(d as HighlightTextBlock);
+        }
+
+
+        private static void ApplyHighlight(HighlightTextBlock tb) {
+            string highlightPhrase = tb.HighlightPhrase;
+            string text = tb.Text;
+
+            if (String.IsNullOrEmpty(highlightPhrase)) {
+                tb.Inlines.Clear();
+
+                tb.Inlines.Add(text);
+            }
+
+            else {
+                int index = text.IndexOf(highlightPhrase, (tb.IsCaseSensitive) ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase);
+
+                tb.Inlines.Clear();
+
+                if (index < 0)
+                    tb.Inlines.Add(text);
+
+                else {
+                    if (index > 0) 
+                        tb.Inlines.Add(text.Substring(0, index));
+
+                    tb.Inlines.Add(new Run(text.Substring(index, highlightPhrase.Length))
+                    {
+                        Background = tb.HighlightBrush
+                    });
+
+                    index += highlightPhrase.Length;
+
+                    if (index < text.Length)
+                        tb.Inlines.Add(text.Substring(index));
+                }
+            }
+        }
+    }
+}
