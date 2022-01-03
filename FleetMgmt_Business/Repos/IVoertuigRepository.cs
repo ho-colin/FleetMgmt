ed0409f427cf52ab3b277de66beec2de87a3fe1e
Colin Meerschman <colin.meerschman@student.hogent.be>
2022-01-03 16:36:54 +0100

    + REFACTOR Repository , Interface & ToString

--- FleetMgmt_Business/Repos/IVoertuigRepository.cs
+++ FleetMgmt_Business/Repos/IVoertuigRepository.cs
@@ -12,12 +12,8 @@ namespace FleetMgmt_Business.Repos {
         Voertuig voegVoertuigToe(Voertuig voertuig);
         void verwijderVoertuig(Voertuig voertuig);
         Voertuig geefVoertuig(string chassisnummer);
-        IEnumerable<Voertuig> toonVoertuigen(string merk, string model, string typeVoertuig, string brandstof,
-            string kleur, int? aantalDeuren, string bestuurderId);
+        IEnumerable<Voertuig> toonVoertuigen(string merk, string model, string typeVoertuig, string brandstof, string kleur, int? aantalDeuren, string bestuurderId);
         bool bestaatVoertuig(string chassisnummer);
-        bool bestaatVoertuig(Voertuig voertuig);
-        void bewerkVoertuig_BestuurderToevoegen(Voertuig voertuig);
-        void bewerkVoertuig_BestuurderVerwijderen(Voertuig voertuig);
-        void bewerkVoertuig_BestuurderWisselen(Voertuig voertuig);
+        void bewerkVoertuig(Voertuig voertuig);
     }
 }
