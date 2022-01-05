0200ea38047bb11f59a34433196f55b75e364d28
Colin Meerschman <colin.meerschman@student.hogent.be>
2022-01-05 14:57:57 +0100

    + heeftRijbewijs methode

---
+++ FleetMgmg_Data/Exceptions/RijbewijsRepositoryException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetMgmg_Data.Exceptions {
+    public class RijbewijsRepositoryException : Exception {
+        public RijbewijsRepositoryException(string message) : base(message) { }
+
+        public RijbewijsRepositoryException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
---
+++ FleetMgmg_Data/Repositories/RijbewijsRepository.cs
@@ -0,0 +1,44 @@
+﻿using FleetMgmg_Data.Exceptions;
+using FleetMgmt_Business.Enums;
+using FleetMgmt_Business.Objects;
+using FleetMgmt_Business.Repos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetMgmg_Data.Repositories {
+    public class RijbewijsRepository : IRijbewijsRepository {
+        public bool heeftRijbewijs(RijbewijsEnum r, Bestuurder b) {
+            string query = "SELECT Count(*) FROM BestuurderRijbewijs WHERE Bestuurder=@bestuurder AND Categorie=@categorie";
+            using(SqlConnection conn = ConnectionClass.getConnection()) {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn)) {
+                    cmd.Parameters.AddWithValue("@bestuurder", b.Rijksregisternummer);
+                    cmd.Parameters.AddWithValue("@categorie", r.ToString());
+
+                    try {
+                        int aantal = (int)cmd.ExecuteScalar();
+                        if (aantal > 0) return true; return false;
+                    } catch (Exception ex) {
+                        throw new RijbewijsRepositoryException(ex.Message,ex);
+                    } finally { conn.Close(); }
+                }
+            }
+        }
+
+        public List<Rijbewijs> toonRijbewijzen(Bestuurder b) {
+            throw new NotImplementedException();
+        }
+
+        public void verwijderRijbewijs(RijbewijsEnum r, Bestuurder b) {
+            throw new NotImplementedException();
+        }
+
+        public void voegRijbewijsToe(Rijbewijs r, Bestuurder b) {
+            throw new NotImplementedException();
+        }
+    }
+}
