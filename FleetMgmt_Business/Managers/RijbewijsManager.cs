4d956643d81e8104cefc71fb9e3da0555e29b4f5
Colin Meerschman <colin.meerschman@student.hogent.be>
2022-01-05 14:27:04 +0100

    + Init Rijbewijs

---
+++ FleetMgmt_Business/Managers/RijbewijsManager.cs
@@ -0,0 +1,35 @@
+﻿using FleetMgmt_Business.Enums;
+using FleetMgmt_Business.Objects;
+using FleetMgmt_Business.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetMgmt_Business.Managers {
+    public class RijbewijsManager : IRijbewijsRepository {
+
+        private IRijbewijsRepository repo;
+
+        public RijbewijsManager(IRijbewijsRepository repo) {
+            this.repo = repo;
+        }
+
+        public bool heeftRijbewijs(RijbewijsEnum r, Bestuurder b) {
+            return repo.heeftRijbewijs(r, b);
+        }
+
+        public List<Rijbewijs> toonRijbewijzen(Bestuurder b) {
+            return repo.toonRijbewijzen(b);
+        }
+
+        public void verwijderRijbewijs(RijbewijsEnum r, Bestuurder b) {
+            repo.verwijderRijbewijs(r, b);
+        }
+
+        public void voegRijbewijsToe(Rijbewijs r, Bestuurder b) {
+            repo.voegRijbewijsToe(r, b);
+        }
+    }
+}
---
+++ FleetMgmt_Business/Repos/IRijbewijsRepository.cs
@@ -0,0 +1,17 @@
+﻿using FleetMgmt_Business.Enums;
+using FleetMgmt_Business.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetMgmt_Business.Repos {
+    public interface IRijbewijsRepository {
+
+        void voegRijbewijsToe(Rijbewijs r, Bestuurder b);
+        void verwijderRijbewijs(RijbewijsEnum r, Bestuurder b);
+        bool heeftRijbewijs(RijbewijsEnum r, Bestuurder b);
+        List<Rijbewijs> toonRijbewijzen(Bestuurder b);
+    }
+}
